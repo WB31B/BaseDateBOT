0de960f6da0f1b55b70daaeec219ee22f5e0a22a
WB31W <pythontermux@gmail.com>
2023-04-11 23:22:59 +0300

    test pq

--- bot.go
+++ bot.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	_ "github.com/lib/pq"
 )
 
 const tgbotapiKey = ""
@@ -16,77 +16,88 @@ var rootMenu = tgbotapi.NewReplyKeyboard(
 	),
 )
 
-var DB *gorm.DB
-
-type User struct {
-	gorm.Model
-	ID      int    `json:"id" gorm:"primary_key"`
-	Name    string `json:"name"`
-	Message string `json:"user_message"`
-}
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "postgres"
+	password = ""
+	dbName   = "tusergbot"
+)
 
 func main() {
-	var (
-		bot        *tgbotapi.BotAPI
-		err        error
-		updChannel tgbotapi.UpdatesChannel
-		update     tgbotapi.Update
-		updConfig  tgbotapi.UpdateConfig
-	)
-	bot, err = tgbotapi.NewBotAPI(tgbotapiKey)
+	// var (
+	// 	bot        *tgbotapi.BotAPI
+	// 	err        error
+	// 	updChannel tgbotapi.UpdatesChannel
+	// 	update     tgbotapi.Update
+	// 	updConfig  tgbotapi.UpdateConfig
+	// )
+	// bot, err = tgbotapi.NewBotAPI(tgbotapiKey)
+	// CheckError(err)
+
+	// updConfig.Timeout = 60
+	// updConfig.Limit = 1
+	// updConfig.Offset = 0
+
+	// updChannel = bot.GetUpdatesChan(updConfig)
+
+	psqlsconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbName)
+
+	db, err := sql.Open("postgres", psqlsconn)
+	CheckError(err)
+
+	defer db.Close()
+
+	insertStmt := `insert into "user"("user_id", "user_name", "user_message") values(44, "dan", "Hi, it is my first message")`
+
+	_, e := db.Exec(insertStmt)
+	CheckError(e)
+
+	// insertDynStmt := `insert into "User" ("Name", "EmpId") values($1, $2)`
+	// _, e = db.Exec(insertDynStmt, "Yankauskas", 04)
+	// CheckError(e)
+
+	// for {
+	// 	update = <-updChannel
+
+	// 	if update.Message != nil {
+	// 		if update.Message.IsCommand() {
+	// 			cmdText := update.Message.Command()
+	// 			if cmdText == "start" {
+	// 				msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi -> "+update.Message.From.FirstName)
+
+	// 				bot.Send(msgConfig)
+	// 			}
+	// 			// } else if cmdText == "menu" {
+	// 			// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Main Menu")
+	// 			// 	msg.ReplyMarkup = mainMenu
+	// 			// 	bot.Send(msg)
+	// 			// }
+	// 		} else {
+	// 			if update.Message.Text == "dRootfaceT1" {
+	// 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+	// 				msg.ReplyMarkup = rootMenu
+	// 				bot.Send(msg)
+	// 			} else {
+	// 				msgInfoUser := fmt.Sprintf("[Your name]: %s\n[Your ID]: %v\n[Your message]: %s\n",
+	// 					update.Message.From.FirstName,
+	// 					update.Message.From.ID,
+	// 					update.Message.Text,
+	// 				)
+
+	// 				msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, msgInfoUser)
+	// 				bot.Send(msgConfig)
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// bot.StopReceivingUpdates()
+}
+
+func CheckError(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
-
-	updConfig.Timeout = 60
-	updConfig.Limit = 1
-	updConfig.Offset = 0
-
-	updChannel = bot.GetUpdatesChan(updConfig)
-
-	db, err := gorm.Open(postgres.Open("postgres://postgres:pG2r4hack@localhost:5432/postgres"), &gorm.Config{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	db.AutoMigrate(&User{})
-
-	DB = db
-
-	for {
-		update = <-updChannel
-
-		if update.Message != nil {
-			if update.Message.IsCommand() {
-				cmdText := update.Message.Command()
-				if cmdText == "start" {
-					msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi -> "+update.Message.From.FirstName)
-
-					bot.Send(msgConfig)
-				}
-				// } else if cmdText == "menu" {
-				// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Main Menu")
-				// 	msg.ReplyMarkup = mainMenu
-				// 	bot.Send(msg)
-				// }
-			} else {
-				if update.Message.Text == "dRootfaceT1" {
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-					msg.ReplyMarkup = rootMenu
-					bot.Send(msg)
-				} else {
-					msgInfoUser := fmt.Sprintf("[Your name]: %s\n[Your ID]: %v\n[Your message]: %s\n",
-						update.Message.From.FirstName,
-						update.Message.From.ID,
-						update.Message.Text,
-					)
-
-					msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, msgInfoUser)
-					bot.Send(msgConfig)
-				}
-			}
-		}
-	}
-
-	bot.StopReceivingUpdates()
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/jackc/pgx/v5 v5.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.7 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11 // indirect
